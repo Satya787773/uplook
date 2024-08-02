cae044dc11998e35b470c07d49e9f7800503da17
Pankaj Sharma <pankaj@webnapp.solutions>
2024-08-02 18:07:00 +0530

    Node setup and connection with backend

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import Header from "./component/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
@@ -5,10 +6,19 @@ import Courses from "./pages/Courses";
 import Careers from "./pages/Careers";
 import Footer from "./component/Footer";
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/message")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
   return (
     <>
       <BrowserRouter>
       <Header />
+      <h1>{message}</h1>
       <Routes>
           <Route index element={<Home />} />
           <Route path="courses" element={<Courses />} />
